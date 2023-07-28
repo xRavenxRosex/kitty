f2e7d1ad35d4fcfa114dd6a6aa0d5a47b4249d48
Raven <131493744+xRavenxRosex@users.noreply.github.com>
2023-07-28 10:21:01 -0400

    Added JS functionality

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+//console.log(button);
+//console.log(cat);
+
+button.addEventListener("click", function () {
+cat.classList.add("show");
+});
